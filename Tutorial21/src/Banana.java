10e63e04927682803636124b71bf204c6b970448
KimHeeSu1203 <ozo789@sookmyung.ac.kr>
2022-01-14 19:40:25 +0900

    220114 / JAVA / 다형성

---
+++ Tutorial21/src/Banana.java
@@ -0,0 +1,7 @@
+public class Banana extends Fruit{
+    public Banana(){
+        name = "바나나";
+        price = 1000;
+        fresh = 50;
+    }
+}
---
+++ Tutorial21/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("복숭아 : 1, 바나나 : 2");
+        int number = scanner.nextInt();
+
+        Fruit fruit;
+        if (number == 1){
+            fruit = new Peach();
+            fruit.show();
+        }
+        else if (number == 2){
+            fruit = new Banana();
+            fruit.show();
+        }
+
+    }
+}
